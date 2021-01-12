380425cf85e65d7b9fdfdea3ceb3d89ba6cd1f39
Josh Ryland <jryland@protonmail.com>
2021-01-12 20:00:23 +1300

    Altered res

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(express.json());
 app.use(cors());
 
 app.get('/', (req,res) => {
-    res.send(database.users);
+    res.send('It \'s working!');
 })
 
 app.post('/signin', signin(db,bcrypt));
